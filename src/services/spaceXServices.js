5fca6a20c762e2c944802035417a75885499a28b
davittrdzv <davittrdzv@gmail.com>
2025-06-10 12:17:41 -0600

    Ticket DxHjJucz: Corregir nombre de servicios SpaceX:
    1.	Agregar “get” al inicio de cada función.

--- src/services/spaceXServices.js
+++ src/services/spaceXServices.js
@@ -3,23 +3,23 @@ import axios from 'axios'
 const BASE_URLV4 = 'https://api.spacexdata.com/v4'
 const BASE_URLV5 = 'https://api.spacexdata.com/v5'
 
-const companySpaceXService = () => axios.get(`${BASE_URLV4}/company`)
+const getCompanySpaceXService = () => axios.get(`${BASE_URLV4}/company`)
 
-const launchesSpaceXService = () => axios.get(`${BASE_URLV5}/launches`)
+const getLaunchesSpaceXService = () => axios.get(`${BASE_URLV5}/launches`)
 
-const rocketsSpaceXService = () => axios.get(`${BASE_URLV4}/rockets`)
+const getRocketsSpaceXService = () => axios.get(`${BASE_URLV4}/rockets`)
 
-const roadsterSpaceXService = () => axios.get(`${BASE_URLV4}/roadster`)
+const getRoadsterSpaceXService = () => axios.get(`${BASE_URLV4}/roadster`)
 
-const starlinkSpaceXService = () => axios.get(`${BASE_URLV4}/starlink`)
+const getStarlinkSpaceXService = () => axios.get(`${BASE_URLV4}/starlink`)
 
-const historySpaceXService = () => axios.get(`${BASE_URLV4}/history`)
+const getHistorySpaceXService = () => axios.get(`${BASE_URLV4}/history`)
 
 export {
-  companySpaceXService,
-  launchesSpaceXService,
-  rocketsSpaceXService,
-  roadsterSpaceXService,
-  starlinkSpaceXService,
-  historySpaceXService
+  getCompanySpaceXService,
+  getLaunchesSpaceXService,
+  getRocketsSpaceXService,
+  getRoadsterSpaceXService,
+  getStarlinkSpaceXService,
+  getHistorySpaceXService
 }
