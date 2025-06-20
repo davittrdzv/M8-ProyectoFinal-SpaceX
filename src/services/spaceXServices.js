7f39aa02ade960078a4325a13afc7feb79c18e39
davittrdzv <davittrdzv@gmail.com>
2025-06-19 18:26:11 -0600

    Ticket lDgJID63: Crear servicios de consumo:
    1.	Creación de servicios de consumo de SpaceX (Crew, Ships, Capsules, Payloads, Cores, Landpads).

--- src/services/spaceXServices.js
+++ src/services/spaceXServices.js
@@ -21,15 +21,27 @@ const getHistorySpaceXService = () => axios.get(`${BASE_URLV4}/history`)
 
 const getLaunchpadsSpaceXService = () => axios.get(`${BASE_URLV4}/launchpads`)
 
+const getCrewSpaceXService = () => axios.get(`${BASE_URLV4}/crew`)
+
 const getOneCrewSpaceXService = (id) => axios.get(`${BASE_URLV4}/crew/${id}`)
+
+const getShipsSpaceXService = () => axios.get(`${BASE_URLV4}/ships`)
 
 const getOneShipSpaceXService = (id) => axios.get(`${BASE_URLV4}/ships/${id}`)
 
+const getCapsulesSpaceXService = () => axios.get(`${BASE_URLV4}/capsules`)
+
 const getOneCapsuleSpaceXService = (id) => axios.get(`${BASE_URLV4}/capsules/${id}`)
+
+const getPayloadsSpaceXService = () => axios.get(`${BASE_URLV4}/payloads`)
 
 const getOnePayloadSpaceXService = (id) => axios.get(`${BASE_URLV4}/payloads/${id}`)
 
+const getCoresSpaceXService = () => axios.get(`${BASE_URLV4}/cores`)
+
 const getOneCoreSpaceXService = (id) => axios.get(`${BASE_URLV4}/cores/${id}`)
+
+const getLandpadsSpaceXService = () => axios.get(`${BASE_URLV4}/landpads`)
 
 const getOneLandpadSpaceXService = (id) => axios.get(`${BASE_URLV4}/landpads/${id}`)
 
@@ -43,11 +55,17 @@ export {
   getStarlinkSpaceXService,
   getHistorySpaceXService,
   getLaunchpadsSpaceXService,
+  getCrewSpaceXService,
   getOneCrewSpaceXService,
+  getShipsSpaceXService,
   getOneShipSpaceXService,
+  getCapsulesSpaceXService,
   getOneCapsuleSpaceXService,
+  getPayloadsSpaceXService,
   getOnePayloadSpaceXService,
+  getCoresSpaceXService,
   getOneCoreSpaceXService,
+  getLandpadsSpaceXService,
   getOneLandpadSpaceXService,
 
 }
