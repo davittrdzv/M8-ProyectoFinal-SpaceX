6fdc82f267b62b023832d237612e824067f54a21
davittrdzv <davittrdzv@gmail.com>
2025-07-07 16:36:44 -0600

    Ticket: 7qZO3pzN: Feature: Filtrado de satélites Starlink en gráfico del planeta:
    1.	Lógica para obtener set de años de lanzamiento, meses de lanzamiento, versiones de satélite, filtros seleccionados.
    2.	Formulario para realizar filtrado.
    3.	Filtrado con solo un valor o varios.

--- src/pages/Starlink.jsx
+++ src/pages/Starlink.jsx
@@ -2,9 +2,74 @@ import EarthGlobe from '@/components/EarthGlobe'
 import Spinner from '@/components/Spinner'
 import { useSpaceXContext } from '@/hooks/useSpaceXContext'
 import SpaceXLogo from '@/components/SpaceXLogo'
+import { useEffect, useState } from 'react'
 
 const Starlink = () => {
   const { starlinkInfo, isStarlinkInfoLoading } = useSpaceXContext()
+
+  const [filteredSatellites, setFilteredSatellites] = useState([])
+  const [launchYear, setLaunchYear] = useState([])
+  const [launchMonth, setLaunchMonth] = useState([])
+  const [satVersion, setSatVersion] = useState([])
+
+  const [selectedYear, setSelectedYear] = useState('')
+  const [selectedMonth, setSelectedMonth] = useState('')
+  const [selectedVersion, setSelectedVersion] = useState('')
+
+  const monthNames = {
+    '01': 'January',
+    '02': 'February',
+    '03': 'March',
+    '04': 'April',
+    '05': 'May',
+    '06': 'June',
+    '07': 'July',
+    '08': 'August',
+    '09': 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December'
+  }
+
+  useEffect(() => {
+    if (!isStarlinkInfoLoading && starlinkInfo.length > 0) {
+      const filteredSats = starlinkInfo.filter(sat => {
+        const date = sat.spaceTrack.LAUNCH_DATE
+        const year = date?.slice(0, 4) ?? 'Unknown Year'
+        const month = date?.slice(5, 7) ?? 'Unknown Month'
+        const version = sat.version ?? 'Unknown Version'
+
+        return (
+          (selectedYear === '' || year === selectedYear) &&
+          (selectedMonth === '' || month === selectedMonth) &&
+          (selectedVersion === '' || version === selectedVersion)
+        )
+      })
+      setFilteredSatellites(filteredSats)
+    }
+  }, [starlinkInfo, isStarlinkInfoLoading, selectedYear, selectedMonth, selectedVersion])
+
+  useEffect(() => {
+    if (!isStarlinkInfoLoading && starlinkInfo.length > 0) {
+      setFilteredSatellites(starlinkInfo)
+
+      const uniqueYears = Array.from(
+        new Set(starlinkInfo.map(sat => sat.spaceTrack.LAUNCH_DATE?.slice(0, 4) ?? 'Unknown Year'))
+      ).sort()
+      setLaunchYear(uniqueYears)
+
+      const uniqueMonths = Array.from(
+        new Set(starlinkInfo.map(sat => sat.spaceTrack.LAUNCH_DATE?.slice(5, 7) ?? 'Unknown Month'))
+      ).sort()
+      setLaunchMonth(uniqueMonths)
+
+      const uniqueVersions = Array.from(
+        new Set(starlinkInfo.map(sat => sat.version ?? 'Unknown Version'))
+      ).sort()
+      setSatVersion(uniqueVersions)
+    }
+  }, [isStarlinkInfoLoading, starlinkInfo])
+
   return (
     <>
       <SpaceXLogo className='logo-spacex-w mb-4 mt-custom mx-auto d-block' />
@@ -13,10 +78,78 @@ const Starlink = () => {
         <p>Starlink is SpaceX’s ambitious satellite internet project, aiming to provide global broadband coverage through a vast network of low Earth orbit satellites.</p>
         <p>This page displays the real-time positions of all active Starlink satellites currently orbiting Earth.</p>
         {isStarlinkInfoLoading
-          ? <Spinner />
+          ? (
+            <Spinner />
+            )
           : (
             <>
-              <EarthGlobe starlinkData={starlinkInfo} />
+              <h4>{starlinkInfo.length} Satellites currently in Orbit</h4>
+              {selectedYear !== '' || selectedMonth !== '' || selectedVersion !== ''
+                ? (
+                  <>
+                    <span>Applied Filters:</span>{' '}
+                    {selectedYear !== '' && <span>Launch Year: {selectedYear}</span>}{' '}
+                    {selectedMonth !== '' && <span>Launch Month: {monthNames[selectedMonth] || selectedMonth}</span>}{' '}
+                    {selectedVersion !== '' && <span>Satellite Version: {selectedVersion}</span>}{' '}
+                    <p>{filteredSatellites.length} Satellites Found</p>
+                  </>
+                  )
+                : null}
+              <form className='container border-custom mb-2'>
+                <div>
+                  <p className='compact-text'>Filter Satellites:</p>
+                  <select
+                    className='m-2'
+                    id='launchYear'
+                    value={selectedYear}
+                    onChange={(e) => setSelectedYear(e.target.value)}
+                  >
+                    <option value=''>Filter by Year of Launch</option>
+                    {launchYear.map(year => (
+                      <option key={year} value={year}>{year}</option>
+                    ))}
+                  </select>
+                  <select
+                    className='m-2'
+                    id='launchMonth'
+                    value={selectedMonth}
+                    onChange={(e) => setSelectedMonth(e.target.value)}
+                  >
+                    <option value=''>Filter by Month of Launch</option>
+                    {launchMonth.map(month => (
+                      <option key={month} value={month}>
+                        {monthNames[month] || month}
+                      </option>
+                    ))}
+                  </select>
+                  <select
+                    className='m-2'
+                    id='satVersion'
+                    value={selectedVersion}
+                    onChange={(e) => setSelectedVersion(e.target.value)}
+                  >
+                    <option value=''>Filter by Satellite Version</option>
+                    {satVersion.map(version => (
+                      <option key={version} value={version}>{version}</option>
+                    ))}
+                  </select>
+                </div>
+                {(selectedYear !== '' || selectedMonth !== '' || selectedVersion !== '') &&
+                  <div>
+                    <button
+                      type='button'
+                      className='btn btn-sm btn-custom m-2'
+                      onClick={() => {
+                        setSelectedYear('')
+                        setSelectedMonth('')
+                        setSelectedVersion('')
+                      }}
+                    >
+                      Reset Filters
+                    </button>
+                  </div>}
+              </form>
+              <EarthGlobe starlinkData={filteredSatellites} />
             </>
             )}
       </div>
