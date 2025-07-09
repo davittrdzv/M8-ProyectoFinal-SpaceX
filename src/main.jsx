044118923dd3ac0164267e36473941cee4894463
davittrdzv <davittrdzv@gmail.com>
2025-07-09 12:49:49 -0600

    Ticket xLWMuroP: Feature: Colapsar NavBar:
    1.	Comentar import 'bootstrap/dist/js/bootstrap.bundle.min.js', ya que afectaba el colapsar menú hamburguesa.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import '@/styles/styles.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
