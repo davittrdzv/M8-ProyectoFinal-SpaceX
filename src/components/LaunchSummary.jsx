3055507aa76be50353a0223731ed0f73cff078d7
davittrdzv <davittrdzv@gmail.com>
2025-07-14 18:19:12 -0600

    Ticket: 6RwkxjAT: Feature: Estilizar página.
    1.	Ajuste de clases y estructura en HTML en LaunchSummary.

--- src/components/LaunchSummary.jsx
+++ src/components/LaunchSummary.jsx
@@ -6,8 +6,44 @@ const LaunchSummary = ({ id, name, date, success, rocket, launchpad, picture }) 
   return (
     <div className='col-md-6 text-center' style={{ width: '20rem' }}>
       <Link to={`/launches/${id}`} className='text-decoration-none text-white'>
-        <h3 className='border-custom'>{name}</h3>
-        <img src={picture || placeholderPic} className='card-img-top' alt={name} onError={handlePicError} />
+        <div
+          className='border-custom mb-2 d-flex align-items-center justify-content-center text-center'
+          style={{
+            height: '4.5rem',
+            padding: '0 0.5rem',
+            display: '-webkit-box',
+            WebkitLineClamp: 2,
+            WebkitBoxOrient: 'vertical',
+            overflow: 'hidden',
+            wordBreak: 'break-word',
+          }}
+          title={name}
+        >
+          <h3 style={{ margin: 0, fontWeight: 'bold' }}>{name}</h3>
+        </div>
+        <div
+          style={{
+            width: '100%',
+            height: '200px',
+            overflow: 'hidden',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginBottom: '1rem',
+          }}
+        >
+          <img
+            src={picture || placeholderPic}
+            alt={name}
+            onError={handlePicError}
+            style={{
+              maxWidth: '100%',
+              maxHeight: '100%',
+              objectFit: 'contain',
+              display: 'block',
+            }}
+          />
+        </div>
       </Link>
       <ul className='list-group list-group-flush'>
         <li className='list-group-item'>
